626cd4e637af425105bd7ddf620620219dd50f38
Gabriel <gabrielmssantiago@gmail.com>
2020-05-01 10:42:50 -0300

    filter inputs and grid system

--- src/components/IssueFilter.js
+++ src/components/IssueFilter.js
@@ -12,6 +12,12 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { red } from '@material-ui/core/colors';
 import { withStyles } from '@material-ui/styles';
 
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+
 const styles = (theme) => ({
   header: {
     backgroundColor: red[500],
@@ -101,29 +107,51 @@ class IssueFilter extends React.Component {
         <ExpansionPanelSummary className={classes.header} expandIcon={<ExpandMoreIcon style={{ color: '#fff' }} />}>
           <Typography variant="h6">Filter</Typography>
         </ExpansionPanelSummary>
-        <ExpansionPanelDetails style={{ display: 'block' }}>
-          <div id="filters" style={{ marginBottom: 20 }}>
-            {'Status: '}
-            <select value={status} onChange={this.onChangeStatus}>
-              <option value="">(All)</option>
-              <option value="New">New</option>
-              <option value="Assigned">Assigned</option>
-              <option value="Fixed">Fixed</option>
-              <option value="Closed">Closed</option>
-            </select>{' '}
-            Effort between: <input size={5} value={effortMin} onChange={this.onChangeEffortMin} />
-            {' - '}
-            <input size={5} value={effortMax} onChange={this.onChangeEffortMax} />
-          </div>
-          <Button
-            style={{ backgroundColor: red[500], color: '#fff' }}
-            variant="contained"
-            type="button"
-            onClick={this.applyFilter}
-          >
-            Apply
-          </Button>{' '}
-          {resetButton}
+        <ExpansionPanelDetails>
+          <Grid container spacing={1}>
+            <Grid item xs={6} sm={4} md={3} lg={1}>
+              <FormControl style={{ marginRight: 20 }}>
+                <InputLabel>Status</InputLabel>
+                <Select native value={status} onChange={this.onChangeStatus}>
+                  <option value="" />
+                  <option value="">All</option>
+                  <option value="New">New</option>
+                  <option value="Assigned">Assigned</option>
+                  <option value="Fixed">Fixed</option>
+                  <option value="Closed">Closed</option>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={6} sm={4} md={3} lg={1}>
+              <TextField
+                style={{ width: 100 }}
+                label="Effort Min"
+                variant="outlined"
+                value={effortMin}
+                onChange={this.onChangeEffortMin}
+              />
+            </Grid>
+            <Grid item xs={6} sm={4} md={3} lg={1}>
+              <TextField
+                style={{ width: 100 }}
+                label="Effort Max"
+                variant="outlined"
+                value={effortMax}
+                onChange={this.onChangeEffortMax}
+              />
+            </Grid>
+            <Grid item xs={6} sm={4} md={3} lg={1}>
+              <Button
+                style={{ backgroundColor: red[500], color: '#fff' }}
+                variant="contained"
+                type="button"
+                onClick={this.applyFilter}
+              >
+                Apply
+              </Button>{' '}
+              {resetButton}
+            </Grid>
+          </Grid>
         </ExpansionPanelDetails>
       </ExpansionPanel>
     );

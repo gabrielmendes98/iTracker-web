62b8938075682aa19b27b585ccd01086823fea55
Gabriel <gabrielmssantiago@gmail.com>
2020-05-04 10:18:49 -0300

    new directory structure

--- browser/index.js
+++ browser/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../src/App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(

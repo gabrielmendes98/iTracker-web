fcc8f040bb80d8ee2962ee7505e3e95e5e896e4c
Gabriel <gabrielmssantiago@gmail.com>
2020-05-04 21:02:56 -0300

    fouc resolved

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import './styles.css';
 
-const App = () => (
-  <div>
-    <div id="main">
-      <NavBar />
-      <div id="page-content">
-        <Routes />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ready: false }; // This one.
+  }
+
+  componentDidMount() {
+    this.setState({ ready: true }); // This one.
+  }
+
+  render() {
+    return (
+      <div style={{ visibility: this.state.ready ? 'visible' : 'hidden' }}>
+        <div id="main">
+          <NavBar />
+          <div id="page-content">
+            <Routes />
+          </div>
+        </div>
+        <Footer />
       </div>
-    </div>
-    <Footer />
-  </div>
-);
+    );
+  }
+}
 
 export default App;
